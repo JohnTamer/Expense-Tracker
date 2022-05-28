f458f468e3a99e0e945b3cf9380f44cdfa58821c
JohnTamerZaki <johntamer98@gmail.com>
2022-05-28 20:04:30 +0200

    user inputs are visible and u can filter by year

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import Expenses from "./components/expences/expences";
 import NewExpence from "./components/newExpense/newExpence";
 // import { Try } from "./components/try/index";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 const App = () => {
-  // const print = (v) => {
-  //   console.log("parent" + v);
-  // };
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const transferHandle = (expense) => {
     console.log(expense);
+    // setExpenses([expense, ...expenses]);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
--- src/components/expenseItem/expenseItem.js
+++ src/components/expenseItem/expenseItem.js
@@ -1,44 +1,20 @@
-// import "./expenseItem.css";
-// import { useState } from "react";
-// const ExpenseItem = (props) => {
-//
-//   const [title, setTitle] = useState(props.title);
-//   const clickHandler = () => {
-//     setTitle("updated!");
-//   };
-//   return (
-//     <div className="expense-item">
-//       <div className="expense-date">
-//         <div className="expense-date__month">{month}</div>
-//         <div className="expense-date__year">{day}</div>
-//         <div className="expense-date__day">{year}</div>
-//       </div>
-//       <div className="expense-item__description">
-//         <h2>{title}</h2>
-//         <div className="expense-item__price">${props.amount}</div>
-//         <button onClick={clickHandler}>change title</button>
-//       </div>
-//     </div>
-//   );
-// };
-// export default ExpenseItem;
 import ExpenseDate from "../date/expenseDate";
 import "./expenseItem.css";
 import Card from "./../card/card";
-import { useState } from "react";
+
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("updated");
-  };
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   setTitle("updated");
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>change title</button>
+      <button>change title</button>
     </Card>
   );
 };
