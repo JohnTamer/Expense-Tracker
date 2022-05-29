32967aa98ba116e742120382d6d30ebf89280946
JohnTamerZaki <johntamer98@gmail.com>
2022-05-29 16:24:18 +0200

    All done

---
+++ src/components/chart/chartBar.js
@@ -0,0 +1,18 @@
+import "./chartBars.css";
+const ChartBar = (props) => {
+  let fillheight = "0%";
+  if (props.maxValue > 0) {
+    fillheight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{ height: fillheight }}>
+          {" "}
+        </div>
+        <div className="chart-bar__label">{props.label}</div>
+      </div>
+    </div>
+  );
+};
+export default ChartBar;
